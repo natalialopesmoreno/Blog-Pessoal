e71c02ae28856a048b66d240008ec0b0e8e458dd
Natália Moreno <natalialopesmoreno@gmail.com>
2020-12-10 19:35:34 -0300

    inserindo os métodos Put e Post

--- angular/blogPessoal/src/app/app.module.ts
+++ angular/blogPessoal/src/app/app.module.ts
@@ -14,6 +14,11 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { OrderModule } from 'ngx-order-pipe';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+
+import { PutTemaComponent } from './put-tema/put-tema.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +30,11 @@ import { PostTemaComponent } from './post-tema/post-tema.component';
     PerfilLateralComponent,
     LoginComponent,
     CadastroComponent,
-    PostTemaComponent
+    PostTemaComponent,
+    PutPostagemComponent,
+    PutTemaComponent,
+    DeletePostagemComponent,
+    DeleteTemaComponent
   ],
   imports: [
     BrowserModule,
